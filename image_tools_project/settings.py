f1246c737e63486306e23e8a78ed3b6a5627578b
Zeeshan Ali <zeeshanali4k2517@gmail.com>
2025-12-27 01:16:50 +0500

    fixing bugs

--- image_tools_project/settings.py
+++ image_tools_project/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-cast=lambda v: [s.strip() for s in v.split(',')]
+ALLOWED_HOSTS = ['*']
+# cast=lambda v: [s.strip() for s in v.split(',')]
 
 
 # Application definition
